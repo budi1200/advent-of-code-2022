9bdfd73f8eff67fc66c84688cc352d0605ec968c
Alen Budimir <7889389+budi1200@users.noreply.github.com>
2022-12-01 13:40:08 +0100

    Day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,7 +1,38 @@
+use std::fs;
+
+fn get_sums() -> Vec<i32> {
+    let calories_str = fs::read_to_string("./data/day1.txt").expect("Failed to read file");
+    let calories_lines = calories_str.lines();
+
+    let mut sums = Vec::<i32>::new();
+
+    let mut tmp = 0;
+    calories_lines.for_each(|f| {
+        if f.len() != 0 {
+            tmp += f.parse::<i32>().expect("Expected a number");
+            return;
+        }
+
+        sums.push(tmp);
+        tmp = 0;
+    });
+
+    return sums;
+}
+
 pub fn day1_1() {
-    println!("tmp");
+    let mut sums = get_sums();
+
+    sums.sort();
+    println!("Biggest sum: {:?}", sums.last().expect("Expected a value"));
 }
 
 pub fn day1_2() {
-    println!("tmp");
+    let mut sums = get_sums();
+
+    sums.sort();
+    sums.reverse();
+
+    let top_three = sums.drain(..3);
+    println!("Top 3 sum: {:?}", top_three.sum::<i32>());
 }
