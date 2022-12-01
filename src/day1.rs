e26ad8cf63f30ab6f9ef4e40821451202d778142
Alen Budimir <7889389+budi1200@users.noreply.github.com>
2022-12-01 11:29:59 +0100

    Initial commit

---
+++ src/day1.rs
@@ -0,0 +1,7 @@
+pub fn day1_1() {
+    println!("tmp");
+}
+
+pub fn day1_2() {
+    println!("tmp");
+}
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+mod day1;
+
+use std::collections::HashMap;
+use std::env;
+
+fn get_available_days<'a>() -> HashMap<&'a str, fn()> {
+    return HashMap::from([
+        ("1.1", day1::day1_1 as fn()),
+        ("1.2", day1::day1_2 as fn())
+    ]);
+}
+
+// Usage: cargo run <day>.<part>
+// Example: cargo run 1.2
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        return println!("Invalid arguments provided \nUsage: cargo run <day>.<part>");
+    }
+
+    let day = &args[1];
+
+    println!("Chosen Day {}", day);
+
+    let completed_days = get_available_days();
+
+    let day_to_run = completed_days.get_key_value(&day as &str);
+
+    if day_to_run == None {
+        return println!("Day not found!");
+    }
+
+    // Run function for selected day
+    day_to_run.unwrap().1();
+}
