b4cf81477004fb30f955e93552cd614b856fea80
Alen Budimir <7889389+budi1200@users.noreply.github.com>
2022-12-05 16:07:59 +0100

    Day 5

---
+++ src/day05.rs
@@ -0,0 +1,96 @@
+use regex::Regex;
+use std::fs;
+
+fn parse_crane(data: &str) -> Vec<Vec<&str>> {
+    let mut cargo: Vec<Vec<&str>> = Vec::new();
+
+    data.lines().rev().skip(1).for_each(|f| {
+        let mut iter = f.split_terminator("").into_iter();
+        let mut index = 0;
+
+        while iter.next().is_some() {
+            // Get group of 3 chars (either three spaces or [X])
+            let group = iter.by_ref().take(3).collect::<Vec<&str>>();
+            // We are interested only in the middle character
+            let value = group.get(1).unwrap();
+
+            // Add empty vector if col is empty
+            if cargo.get(index).is_none() {
+                cargo.push(Vec::new());
+            }
+
+            // Push box to current col
+            if *value != " " {
+                cargo[index].push(value);
+            }
+
+            index += 1;
+        }
+    });
+
+    //    println!("{:?}", cargo);
+    return cargo;
+}
+
+pub fn day05_1() {
+    let data_str = fs::read_to_string("./data/day05.txt").expect("Failed to read file");
+    let sections = data_str.split_terminator("\n\n").collect::<Vec<&str>>();
+    let moves = sections.last().unwrap();
+
+    let mut cargo = parse_crane(sections.first().unwrap()).clone();
+    let re = Regex::new(r"([0-9]+)").unwrap();
+
+    moves.lines().for_each(|f| {
+        //        println!("{:?}", f);
+        let parsed_moves = re
+            .find_iter(f)
+            .map(|cap| cap.as_str().parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
+
+        let source_col = cargo.get_mut(parsed_moves[1] - 1).unwrap();
+        let cache = source_col.split_off(source_col.len() - parsed_moves[0]);
+        let mut reversed = cache.into_iter().rev().collect::<Vec<&str>>();
+
+        cargo.get_mut(parsed_moves[2] - 1).unwrap().append(&mut reversed);
+    });
+
+    let mut output = String::new();
+
+    cargo.iter().for_each(|f| {
+        output += f.last().unwrap();
+    });
+
+    //    println!("{:?}", cargo);
+    println!("{:?}", output);
+}
+
+pub fn day05_2() {
+    let data_str = fs::read_to_string("./data/day05.txt").expect("Failed to read file");
+    let sections = data_str.split_terminator("\n\n").collect::<Vec<&str>>();
+    let moves = sections.last().unwrap();
+
+    let mut cargo = parse_crane(sections.first().unwrap()).clone();
+    let re = Regex::new(r"([0-9]+)").unwrap();
+
+    moves.lines().for_each(|f| {
+        //        println!("{:?}", f);
+        let parsed_moves = re
+        .find_iter(f)
+        .map(|cap| cap.as_str().parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+
+        let source_col = cargo.get_mut(parsed_moves[1] - 1).unwrap();
+        let mut cache = source_col.split_off(source_col.len() - parsed_moves[0]);
+
+        cargo.get_mut(parsed_moves[2] - 1).unwrap().append(&mut cache);
+    });
+
+    let mut output = String::new();
+
+    cargo.iter().for_each(|f| {
+        output += f.last().unwrap();
+    });
+
+    //    println!("{:?}", cargo);
+    println!("{:?}", output);
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod day01;
 mod day02;
 mod day03;
 mod day04;
+mod day05;
 
 use std::collections::HashMap;
 use std::env;
@@ -15,8 +16,10 @@ fn get_available_days<'a>() -> HashMap<&'a str, fn()> {
         ("3.1", day03::day03_1 as fn()),
         ("3.2", day03::day03_2 as fn()),
         ("4.1", day04::day04_1 as fn()),
-        ("4.2", day04::day04_2 as fn())
-        ]);
+        ("4.2", day04::day04_2 as fn()),
+        ("5.1", day05::day05_1 as fn()),
+        ("5.2", day05::day05_2 as fn()),
+    ]);
 }
 
 // Usage: cargo run <day>.<part>
